510a83207cd940c65c784109100c593d8e4d192d
veraspabienhoa <veraspabienhoa@gmail.com>
2026-08-31 12:33:54 +0700

    Kiểm thử giờ vào ca đã điều chỉnh theo hỗ trợ

--- tests/test_attendance_v42.py
+++ tests/test_attendance_v42.py
@@ -2,6 +2,7 @@ from datetime import date, datetime
 import unittest
 
 from vera_attendance_rules import apply_break_restriction, departure_status_is_final
+from vera_web_v2_snapshot import _apply_support_shift_start, _norm
 
 
 class DepartureStatusFinalTests(unittest.TestCase):
@@ -50,6 +51,44 @@ class DepartureStatusFinalTests(unittest.TestCase):
         self.assertTrue(restricted["break_enabled"])
         self.assertEqual(restricted["break_planned_minutes"], 90)
         self.assertEqual(restricted["break_restricted_reason"], "đi trễ")
+
+
+class SupportedShiftStartTests(unittest.TestCase):
+    def setUp(self):
+        self.reason = "Hỗ trợ Ca 1 đi trễ 2 tiếng"
+        self.allowances = {(date(2026, 8, 31), _norm("Mỹ Duyên")): (120, self.reason)}
+
+    def test_checkin_before_supported_start_is_on_time(self):
+        item = {
+            "date": "31/08/2026",
+            "employee_name": "Mỹ Duyên",
+            "shift_start": "10:00",
+            "check_in": "31/08/2026 10:24:06",
+            "late_minutes": 24,
+            "arrival_status": "Đi trễ",
+        }
+
+        adjusted = _apply_support_shift_start(item, self.allowances)
+
+        self.assertEqual(adjusted["shift_start"], "12:00")
+        self.assertEqual(adjusted["late_minutes"], 0)
+        self.assertEqual(adjusted["arrival_status"], "Đúng giờ")
+
+    def test_checkin_after_supported_start_uses_new_late_minutes(self):
+        item = {
+            "date": "31/08/2026",
+            "employee_name": "Mỹ Duyên",
+            "shift_start": "10:00",
+            "check_in": "31/08/2026 12:02:31",
+            "late_minutes": 122,
+            "arrival_status": "Đi trễ",
+        }
+
+        adjusted = _apply_support_shift_start(item, self.allowances)
+
+        self.assertEqual(adjusted["shift_start"], "12:00")
+        self.assertEqual(adjusted["late_minutes"], 2)
+        self.assertEqual(adjusted["arrival_status"], "Đi trễ")
 
 
 if __name__ == "__main__":
