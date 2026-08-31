b9e6dd220bebd8e587764124da864f35b2154884
veraspabienhoa <veraspabienhoa@gmail.com>
2026-08-31 16:02:47 +0700

    Start automatic purchase reconciliation alert watcher

--- web-v2/src/main.jsx
+++ web-v2/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { startPurchaseReconcileAlertWatcher } from './lib/purchaseReconcileAlerts'
 import { registerVeraServiceWorker } from './lib/pushNotifications'
 import './styles.css'
 
 void registerVeraServiceWorker().catch(() => {})
+startPurchaseReconcileAlertWatcher()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
