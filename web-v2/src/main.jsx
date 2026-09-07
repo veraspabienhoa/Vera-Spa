ef8ad9772ff64c9eb270fe6779e37c071a01d4ed
veraspabienhoa <veraspabienhoa@gmail.com>
2026-09-07 16:00:49 +0700

    fix: activate leave date and employee toolbar recovery

--- web-v2/src/main.jsx
+++ web-v2/src/main.jsx
@@ -12,7 +12,9 @@ import { startEmployeeProfileSwitchGuard } from './lib/employeeProfileSwitchGuar
 import { startEmployeeProfileCompletionAndIssuerFix } from './lib/employeeProfileCompletionAndIssuerFix'
 import { startEmployeeDirectoryUx } from './lib/employeeDirectoryUx'
 import { startEmployeeProfileHeaderSaveFix } from './lib/employeeProfileHeaderSaveFix'
+import { startEmployeeToolbarRecovery } from './lib/employeeToolbarRecovery'
 import { startEmployeeMissingProfileFix } from './lib/employeeMissingProfileFix'
+import { startLeaveListDateFilterSync } from './lib/leaveListDateFilterSync'
 import { startDepartmentSalaryAdvanceTransportGuard } from './lib/departmentSalaryAdvanceTransportGuard'
 import { startDepartmentSalaryAdvanceLedger } from './lib/departmentSalaryAdvanceLedger'
 import { startAttendanceTimesoftRefreshGuard } from './lib/attendanceTimesoftRefreshGuard'
@@ -29,7 +31,9 @@ startEmployeeProfileSwitchGuard()
 startEmployeeProfileCompletionAndIssuerFix()
 startEmployeeDirectoryUx()
 startEmployeeProfileHeaderSaveFix()
+startEmployeeToolbarRecovery()
 startEmployeeMissingProfileFix()
+startLeaveListDateFilterSync()
 startDepartmentSalaryAdvanceTransportGuard()
 startDepartmentSalaryAdvanceLedger()
 startAttendanceTimesoftRefreshGuard()
