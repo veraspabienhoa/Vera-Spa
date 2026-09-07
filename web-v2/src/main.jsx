970700f20ebeb60baf34be8c2bf4010e9e0664f3
veraspabienhoa <veraspabienhoa@gmail.com>
2026-09-07 08:37:15 +0700

    feat: enable incomplete-field highlighting and CCCD viewer [deploy-vps]

--- web-v2/src/main.jsx
+++ web-v2/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { startPurchaseReconcileAlertWatcher } from './lib/purchaseReconcileAlerts'
 import { registerVeraServiceWorker } from './lib/pushNotifications'
+import { startEmployeeProfileUxEnhancements } from './lib/employeeProfileUx'
 import './styles.css'
 import './visibility-cleanup.css'
 
 void registerVeraServiceWorker().catch(() => {})
 startPurchaseReconcileAlertWatcher()
+startEmployeeProfileUxEnhancements()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
