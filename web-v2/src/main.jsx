e604880b1c50321de80970b1708bfcafd89eaeb4
veraspabienhoa <veraspabienhoa@gmail.com>
2026-09-07 09:39:48 +0700

    fix: activate exact profile missing labels and CCCD issuer catalog

--- web-v2/src/main.jsx
+++ web-v2/src/main.jsx
@@ -9,6 +9,7 @@ import { startEmployeeCccdFieldExtract } from './lib/employeeCccdFieldExtract'
 import { startEmployeeCccdTabViewer } from './lib/employeeCccdTabViewer'
 import { startEmployeeProfileProductionFix } from './lib/employeeProfileProductionFix'
 import { startEmployeeProfileSwitchGuard } from './lib/employeeProfileSwitchGuard'
+import { startEmployeeProfileCompletionAndIssuerFix } from './lib/employeeProfileCompletionAndIssuerFix'
 import './styles.css'
 import './visibility-cleanup.css'
 
@@ -19,6 +20,7 @@ startEmployeeCccdFieldExtract()
 startEmployeeCccdTabViewer()
 startEmployeeProfileProductionFix()
 startEmployeeProfileSwitchGuard()
+startEmployeeProfileCompletionAndIssuerFix()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
