240cabf81f2ac168a98cc29de56cff5d167716f9
veraspabienhoa <veraspabienhoa@gmail.com>
2026-09-07 09:23:49 +0700

    fix: activate production employee profile safeguards

--- web-v2/src/main.jsx
+++ web-v2/src/main.jsx
@@ -7,6 +7,7 @@ import { registerVeraServiceWorker } from './lib/pushNotifications'
 import { startEmployeeProfileUxEnhancements } from './lib/employeeProfileUx'
 import { startEmployeeCccdFieldExtract } from './lib/employeeCccdFieldExtract'
 import { startEmployeeCccdTabViewer } from './lib/employeeCccdTabViewer'
+import { startEmployeeProfileProductionFix } from './lib/employeeProfileProductionFix'
 import './styles.css'
 import './visibility-cleanup.css'
 
@@ -15,6 +16,7 @@ startPurchaseReconcileAlertWatcher()
 startEmployeeProfileUxEnhancements()
 startEmployeeCccdFieldExtract()
 startEmployeeCccdTabViewer()
+startEmployeeProfileProductionFix()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
